ca5b3849a4fdd57a019699f7860702d9df3bbea4
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-11-17 19:01:57 -0300

    Adicionado imagem para a tela de login e criado a tela de login

--- frontend/catalogo-receitas/src/pages/Login/Login.jsx
+++ frontend/catalogo-receitas/src/pages/Login/Login.jsx
@@ -1,9 +1,47 @@
 import Navbar from "../../components/Navbar/Navbar";
-
+import './Login.css'
+import loginImage from '../../assets/images/background/login.svg'
 const Login = () => {
     return(
         <>
             <Navbar />
+            <div className="container-login">
+                <div className="img-box">
+                    <img src={loginImage} alt="imagem" />
+                </div>
+
+                <div className="content-box">
+                    <div className="form-box">
+                        <h2>LOGIN</h2>
+                        <form action="">
+                            <div className="input-box">
+                                <span>Email</span>
+                                <input type="email" name="email" id="email" placeholder="Email" />
+                            </div>
+
+                            <div className="input-box">
+                                <span>Senha</span>
+                                <input type="senha" name="senha" id="senha" placeholder="Senha" />
+                            </div>
+
+                            <div className="remember">
+                                <label>
+                                    <input type="checkbox" name="remember" id="remember" /> Lembre-se
+                                </label>
+                                <a href="">Esqueceu sua senha?</a>
+                            </div>
+
+                            <div className="input-box submit" >
+                                <input type="submit" value="Entrar" />
+                            </div>
+
+                            <div className="input-box">
+                                <p>Não tem uma conta? <a href="">Cadastre-se</a></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
