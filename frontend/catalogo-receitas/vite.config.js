7b6df2b74f57eef57fd830608264228d7896bb2a
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-12-01 21:55:45 -0300

    teste

--- frontend/catalogo-receitas/vite.config.js
+++ frontend/catalogo-receitas/vite.config.js
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT,  
-    host: '0.0.0.0',  
+    port: process.env.PORT, 
+    host: '0.0.0.0', 
   },
-  base: '/',  
-});
+})
