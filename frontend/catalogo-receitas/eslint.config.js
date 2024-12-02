19b94e498429d7d6c4835a08c1a19b4abc8fe4bc
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2024-12-01 21:31:15 -0300

    configuração de variaveis de ambiente no frontend

--- frontend/catalogo-receitas/eslint.config.js
+++ frontend/catalogo-receitas/eslint.config.js
@@ -10,7 +10,10 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node, // Adiciona as variáveis globais do Node.js
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
--- frontend/catalogo-receitas/vite.config.js
+++ frontend/catalogo-receitas/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173, // Usa a variável PORT do Render ou a porta local 5173
+  },
 })
